4a2b5b710dfb6aa25a6bd9d1105cc574986a0353
Nilaycloudengineer <110720184+Nilaycloudengineer@users.noreply.github.com>
2023-10-13 00:40:12 +0530

    Add files via upload
    api

---
+++ api/APIGateway/src/main/java/com/lulu/lulu_02_API_Gateway/Lulu02ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lulu.lulu_02_API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lulu02ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lulu02ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api/APIGateway/src/test/java/com/lulu/lulu_02_API_Gateway/Lulu02ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lulu.lulu_02_API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lulu02ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
